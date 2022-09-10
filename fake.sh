b8c686450769c76c0e6c3a988a0ffdc760221671
smicsolution <smic1774@gmail.com>
2022-09-10 08:27:44 -0400

    initial commit

---
+++ fake.sh
@@ -0,0 +1,15 @@
+git filter-branch -f --env-filter '
+OLD_EMAIL="1codingguy@gmail.com"
+CORRECT_NAME="smicsolution"
+CORRECT_EMAIL="smic1774@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
